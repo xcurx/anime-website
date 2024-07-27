f49c7701bc8fa247daa3b789b4044703037c2d0e
xcurx <suj5al78@gmail.com>
2024-07-27 14:41:58 +0530

    ui errors

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -7,7 +7,7 @@ function AnimeList({info}) {
     <div className='w-full mt-2 p-2'> 
       <MiniCarousel info={info}/>
       
-      <div className='grid xl:grid-cols-4 md:grid-cols-2 mt-5 p-2 xl:'>
+      <div className='grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 mt-5 p-2'>
           <List label={'Top Airing'} listName={'top-airing'}/>
           <List label={'Most Popular'} listName={'most-popular'}/> 
           <List label={'Most Favorite'} listName={'most-favorite'}/> 
