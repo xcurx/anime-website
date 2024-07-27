aa5167bb916c56582789918857b6334146793567
xcurx <suj5al78@gmail.com>
2024-07-27 13:30:18 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/anime':'https://aniwatch-api-woad-five.vercel.app',
+      '/anime':{
+        target:'https://aniwatch-api-woad-five.vercel.app',
+        changeOrigin:true,
+        secure:false,
+      },
       'ajax':'https://hianime.to',
       '/watch':'https://hianime.to'
     }
